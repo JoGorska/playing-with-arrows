6aa2fcd5dde7f55bae721a48d2dea58a3db60717
Joanna Gorska <gorska919@gmail.com>
2021-10-07 14:03:39 +0000

    Renamed functions so they reflect their format

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,26 +5,31 @@
 
 // Vanilla JavaScript Function
 
-// function addNumbers (a, b) {
-//     return a + b
-// };
+function addNumbersVanila (a, b) {
+    return a + b
+};
 
-// let sum = addNumbers (2, 2);
+let sumVanila = addNumbersVanila (2, 2);
 
-// console.log(sum)
+console.log(`sum Vanila javascript ${sumVanila}`)
 
 // Arrow Function With Parameters
 
 
-const addNumbers = (a, b) => {
+const addNumbersArrow = (a, b) => {
     return a + b;
 }
 
-let sum = addNumbers(3, 5)
-console.log (sum);
+let sumArrow = addNumbersArrow(3, 5)
+console.log(`sum Arrow function ${sumArrow}`)
 
 
 // Single Line Arrow Function With Parameters
+
+const addTwoNumbersOneLine = (a, b) => a + b;
+
+let sumOneLine = addTwoNumbersOneLine(5, 10);
+console.log(`sum One line arrow function ${sumOneLine}`)
 
 // Implicit Returns
 
